2b79bde22a2a71ca2a3da893f319efe3eaeff3f5
Dominik Resl <nikicresl@gmail.com>
2019-08-10 16:53:41 +0200

    Update for new deploy

--- leonardo_form_roudnyresl/__init__.py
+++ leonardo_form_roudnyresl/__init__.py
@@ -2,50 +2,56 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+try:
+    from local_settings import APPS
+except ImportError:
+    pass
+
 default_app_config = 'leonardo_form_roudnyresl.Config'
 
 
 class Default(object):
 
-    optgroup = 'Roudnyresl form'
+    if 'leonardo_form_roudnyresl' in APPS:
+        optgroup = 'Roudnyresl form'
 
-    apps = [
-        'leonardo_form_roudnyresl'
-    ]
+        apps = [
+            'leonardo_form_roudnyresl'
+        ]
 
-    widgets = [
-        'leonardo_form_roudnyresl.widget.roudnyreslform.models.RoudnyreslFormWidget'
-    ]
-    
-    config = {
-        'ORDER_DEFAULT_TO_EMAIL':
-        ('to@email.com', u"E-mail, na který se budou odesílat objednávky."),
-        'CHOICES_TYPE_PRODUCT': ('', 'Tuple choices'),
-        'CHOICES_TLOUSTKY_PODSTAVY': ('', 'Tuple choices'),
-        'CHOICES_VYSKA_RELIEFU': ('', 'Tuple choices'),
-        'CHOICES_DOPRAVA': ('', 'Tuple choices'),
-        'CHOICES_PLATBA': ('', 'Tuple choices'),
-        'INFO_PRODUCT_ORDER': ('', 'Info text'),
-        'INFO_NEW_CUSTOMERS': ('', 'Info for new customers'),
-    }
+        widgets = [
+            'leonardo_form_roudnyresl.widget.roudnyreslform.models.RoudnyreslFormWidget'
+        ]
 
-    @property
-    def plugins(self):
-        return [
-            ('leonardo_form_roudnyresl.apps.roudnyresl', 'Roudnyresl form'),
-    ]
+        config = {
+            'ORDER_DEFAULT_TO_EMAIL':
+            ('to@email.com', u"E-mail, na který se budou odesílat objednávky."),
+            'CHOICES_TYPE_PRODUCT': ('', 'Tuple choices'),
+            'CHOICES_TLOUSTKY_PODSTAVY': ('', 'Tuple choices'),
+            'CHOICES_VYSKA_RELIEFU': ('', 'Tuple choices'),
+            'CHOICES_DOPRAVA': ('', 'Tuple choices'),
+            'CHOICES_PLATBA': ('', 'Tuple choices'),
+            'INFO_PRODUCT_ORDER': ('', 'Info text'),
+            'INFO_NEW_CUSTOMERS': ('', 'Info for new customers'),
+        }
 
-    js_files = [
-        'formset/jquery.formset.js',
-        'js/bootstrap-select.min.js',
-    ]
-    
-    css_files = [
-        'css/bootstrap-select.min.css',
-        'css/component.css',
-    ]
+        @property
+        def plugins(self):
+            return [
+                ('leonardo_form_roudnyresl.apps.roudnyresl', 'Roudnyresl form'),
+        ]
 
-    public = True
+        js_files = [
+            'formset/jquery.formset.js',
+            'js/bootstrap-select.min.js',
+        ]
+
+        css_files = [
+            'css/bootstrap-select.min.css',
+            'css/component.css',
+        ]
+
+        public = True
 
 
 class Config(AppConfig, Default):
